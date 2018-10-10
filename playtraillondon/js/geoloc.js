d83573e87fa32cfd562bab6ccade66dcf864db7f
vsigno <v.signorelli@ucl.ac.uk>
2018-10-10 15:26:01 +0100

    json

--- playtraillondon/js/geoloc.js
+++ playtraillondon/js/geoloc.js
@@ -5,11 +5,17 @@
 function parsedLocation()
 {
 	$.getJSON("resources/locations.json", function(json_loc) {
-    console.log(json); // this will show the info it in firebug console
-});
+		
+		console.log(json_loc);
+	
 	var JSON_Loc=JSON.parse(json_loc);
 	
 	for (var i = 0; i < JSON_Loc.locations.length; i++) {
     var locations = JSON_Loc.locations[i];
-    console.log(locations.loc_name);
-}}
+	
+    console.log(locations.loc_name);	
+	}	
+		});
+	
+	
+}
