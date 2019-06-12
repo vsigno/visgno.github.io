29bcd4a504a0b58b35a6a4ea1997551d2bf4e642
vsigno <vale.signorelli@gmail.com>
2019-06-12 12:29:19 +0100

    upd Dubv4 and Playspace

--- playspace/js/playmarkersmng.js
+++ playspace/js/playmarkersmng.js
@@ -3,13 +3,9 @@ var markerDict=new Object;
 var markersURLArray=[];
 var markersNameArray=[];
 
+var videoURLArray=[];
+
 var checktap=false;
-
-let angle = 3 * Math.PI / 180;
-let cx = 0;
-let cy = 0;
-let radius = 2;
-
 
 
 AFRAME.registerComponent('markers_start',{
@@ -20,22 +16,28 @@ AFRAME.registerComponent('markers_start',{
 
 		for(var i=1; i<17; i++)
 		{
-			var url="resources/markers/new_May/pattern-Individual_Blocks-"+i+".patt";
+			var url="resources/markers/pattern-Individual_Blocks-"+i+".patt";
 			markersURLArray.push(url);
 			markersNameArray.push('Marker_'+i);
 			console.log(url);
+
+			var urlvideo="resources/videos/Video_Marker_"+i+".mp4"
+			videoURLArray.push(urlvideo);
+
 		}
 
-			var sceneEl = document.querySelector('a-scene');
 
-			//var k=0;
+			var sceneEl = document.querySelector('a-scene');
+			var storeVideo=sceneEl.querySelector('#storevideo')
+
+
 			for(var k=0; k<16; k++)
 			{
 			var markerEl = document.createElement('a-marker');
 			markerEl.setAttribute('type','pattern');
 			markerEl.setAttribute('url',markersURLArray[k]);
 			markerEl.setAttribute('id',markersNameArray[k]);
-			//markerEl.setAttribute('id','markerswords');
+
 
 			markerEl.setAttribute('registerevents','');
 			sceneEl.appendChild(markerEl);
@@ -49,135 +51,61 @@ AFRAME.registerComponent('markers_start',{
 			textEl.setAttribute('text',{color: 'red', align: 'center', value:markersNameArray[k], width: '5.5'});
 
 			markerEl.appendChild(textEl);
+
+			//Adding a dummy videos
+
+			var videoEl=document.createElement('video');
+			videoEl.setAttribute('id','Video_Marker_'+(k+1));
+			videoEl.setAttribute('src',videoURLArray[k]);
+			videoEl.setAttribute('loop','');
+			videoEl.setAttribute('muted','');
+			videoEl.setAttribute('webkit-playsinline', '');
+			videoEl.setAttribute('playsinline','');
+
+			console.log(videoEl);
+			storeVideo.appendChild(videoEl);
+
+
+			var videoEntity=document.createElement('a-video');
+			videoEntity.setAttribute('id','video'+(k+1));
+			videoEntity.setAttribute('class','video');
+			videoEntity.setAttribute('position',{x: 0, y: 0.583, z: 0});
+			videoEntity.setAttribute('rotation',{x:0, y: 0, z: 0});
+			videoEntity.setAttribute('material',{shader:'chromakey',src:'#Video_Marker_'+(k+1),color:'0 1 0'});
+//			console.log(videoEntity);
+			markerEl.appendChild(videoEntity);
+
+
 		}
 
-
-
-
-
 	}
 });
-
-
-
-
-AFRAME.registerComponent('markInit', {
-	init:function (){
-
-		console.log('test');
-
-
-	markerDict.markerepsilon="false";
-	markerDict.markerhydra="false";
-	markerDict.markershield="false";
-	markerDict.markerswords="false";
-	markerDict.markertoycar="false";
-
-	console.log(markerDict.markertoycar);
-	}
-});
-
 
 
 AFRAME.registerComponent('registerevents', {
 		init: function () {
-
-
-
-			this.tick = AFRAME.utils.throttleTick(this.tick, 150, this);
-
 			const marker = this.el;
 
 			marker.addEventListener("markerFound", ()=> {
 				var markerId = marker.id;
 					console.log('markerFound', markerId);
 
+					marker.emit('IamReady',{value:markerId});
 
-
-
-				if(markerId=='markershield')
-				{
-					markerDict.markershield="true";
-
-					console.log('you found the image');
 
 					var sceneEl = document.querySelector('a-scene');
 
-
-				}
-
-
-				if(markerId=='markerhydra')
-				{
-					markerDict.markerhydra="true";
-
-					console.log('you found the sound');
-
-					var sceneEl = document.querySelector('a-scene');
-
-					sceneEl.querySelector('#thesound').components.sound.playSound();
-				}
-
-
-				if(markerId=='markertoycar')
-				{
-					markerDict.markertoycar="true";
-
-					console.log('you found the 3D model');
-
-					var sceneEl = document.querySelector('a-scene');
-
-				}
-
-
-
-				if(markerId=='markerepsilon')
-				{
-					markerDict.markerepsilon="true";
-
-					console.log('you found the video');
-
-					var sceneEl = document.querySelector('a-scene');
-
-					var device=navigator.platform;
-
-					if(sceneEl.querySelector('#chromaVideo').paused==true)
+					if(sceneEl.querySelector('#Video_'+markerId).paused==true)
 					{
-						sceneEl.querySelector('#chromaVideo').play();
-						sceneEl.querySelector('#chromaVideo').muted=false;
+						sceneEl.querySelector('#Video_'+markerId).play();
+						sceneEl.querySelector('#Video_'+markerId).muted=false;
 					}
 					else
 					{
-					sceneEl.querySelector('#chromaVideo').load();
-					sceneEl.querySelector('#chromaVideo').play();
-					sceneEl.querySelector('#chromaVideo').muted=false;
+					sceneEl.querySelector('#Video_'+markerId).load();
+					sceneEl.querySelector('#Video_'+markerId).play();
+					sceneEl.querySelector('#Video_'+markerId).muted=false;
 					}
-
-				}
-
-
-
-				if(markerId=='markerswords')
-				{
-					markerDict.markerswords="true";
-
-					console.log('you found the video');
-
-					var sceneEl = document.querySelector('a-scene');
-
-					if(sceneEl.querySelector('#mocapVideo').paused==true)
-					{
-						sceneEl.querySelector('#mocapVideo').play();
-						sceneEl.querySelector('#mocapVideo').muted=false;
-					}
-					else
-					{
-					sceneEl.querySelector('#mocapVideo').load();
-					sceneEl.querySelector('#mocapVideo').play();
-					sceneEl.querySelector('#mocapVideo').muted=false;
-					}
-				}
-
 
 			});
 
@@ -185,26 +113,14 @@ AFRAME.registerComponent('registerevents', {
 				var markerId = marker.id;
 				console.log('markerLost', markerId);
 
-	//			var sceneEl = document.querySelector('a-scene');
+				var sceneEl = document.querySelector('a-scene');
 
-		/*		if(sceneEl.querySelector('#thesound').components.sound.isPlaying==true){
-				sceneEl.querySelector('#thesound').components.sound.stopSound();
-				console.log("Stop the sound");}
-*/
-			//	device=navigator.platform;
-
-				//		sceneEl.querySelector('#chromaVideo').pause();
-
-
-
-			//	sceneEl.querySelector('#mocapVideo').pause();
+				if(sceneEl.querySelector('#Video_'+markerId).paused==false)
+				{
+					sceneEl.querySelector('#Video_'+markerId).pause();
+				}
 
 			});
 		},
-
-	tick:function(time,timeDelta) {
-
-
-		}
 
 	});
