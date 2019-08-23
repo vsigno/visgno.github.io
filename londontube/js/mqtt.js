e114c814187c68661e2c4bbce2143c9f4902222b
vsigno <vale.signorelli@gmail.com>
2019-08-23 17:27:22 +0100

    Update mqtt.js

--- londontube/js/mqtt.js
+++ londontube/js/mqtt.js
@@ -7,7 +7,7 @@ AFRAME.registerComponent('mqttreader', {
       var mqtt;
       var reconnectedTimeout=2000;
       var hostname="10.0.1.167";
-      var port=8081;
+      var port=1883;
 
       function onConnect()
       {
@@ -19,17 +19,20 @@ AFRAME.registerComponent('mqttreader', {
       {
         console.log("Connected to "+hostname+" "+ port);
 
-        //mqtt=new Paho.Client("10.0.1.167",8081,"clientjs");
+        mqtt=new Paho.MQTT.Client("10.0.1.167",9001,"clientjs");
 
-        mqtt = new Paho.MQTT.Client("wss://10.0.1.167/mqtt", "clientid");
+        //mqtt = new Paho.MQTT.Client("wss://10.0.1.167/test", "clientid");
 
         var options={
           timeout:3,
           onSuccess:onConnect,
           onFailure:onFailure,
+
         };
 
+
         mqtt.onMessageArrived=onMessageArrived;
+
         mqtt.connect(options);
       }
 
@@ -42,7 +45,7 @@ AFRAME.registerComponent('mqttreader', {
 
       function onMessageArrived(msg)
       {
-        out_msg="msg is "+msg.playloadString+"<br>";
+        out_msg="msg is "+msg.payloadString+"<br>";
         console.log(out_msg);
 
       }
